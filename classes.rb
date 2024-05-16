4afa43e5d7c88de8fa8eecfeaa9a838555fa3bdf
Mihir Kumar <mihirpandey.13@gmail.com>
2024-05-16 12:54:26 +0000

    Added student class with more syntax stuff

--- classes.rb
+++ classes.rb
@@ -1,17 +1,47 @@
 class User
   attr_accessor :name, :email
+
   def initialize(name, email)
     @name = name
     @email = email
   end
+
   def run
     puts "Hey I'm running"
   end
+
   def self.identify_yourself
-    puts "Hey I am a class method"
+    puts 'Hey I am a class method'
   end
 end
-user = User.new("mashrur", "mashrur@example.com")
+user = User.new('mashrur', 'mashrur@example.com')
 user.run
-User.identify_youself # to run this class method you don't need an instance of user 
-                      # you can directly call the class User 
+User.identify_youself # to run this class method you don't need an instance of user
+# you can directly call the class User
+
+class Student
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, username, email, password)
+    @first_name = firstname
+    @last_name = lastname
+    @username = username
+    @email = email
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}, Last name: #{@last_name}, Username: #{@username},
+                  email address: #{@email}"
+  end
+end
+
+mashrur = Student.new('Mashrur', 'Hossain', 'mashrur1', 'mashrur@example.com',
+                      'password1')
+john = Student.new('John', 'Doe', 'john1', 'john1@example.com',
+                   'password2')
+puts mashrur
+puts john
+mashrur.last_name = john.last_name
+puts 'Mashrur is altered'
+puts mashrur
