d0edf9c2647e8c1d52628a3301779438de4ed399
Mihir Kumar <mihirpandey.13@gmail.com>
2024-05-31 17:07:52 +0000

    Added bcrypt & modules

--- authentication.rb
+++ authentication.rb
@@ -1,3 +1,12 @@
+require 'bundler/inline'
+
+gemfile true do
+  source 'http://rubygems.org'
+  gem 'bcrypt'
+end
+
+require 'bcrypt'
+
 # users array where username and password are stored
 users = [
   { username: 'mashrur', password: 'password1' },
@@ -37,3 +46,20 @@ while attempts < 4
   attempts += 1
 end
 puts 'You have exceeded the number of attempts' if attempts == 4
+
+def create_hash_digest(password)
+  BCrypt::Password.create(password)
+end
+
+def verify_hash_digest(password)
+  BCrypt::Password.new(password)
+end
+
+def create_secure_users(list_of_users)
+  list_of_users.each do |user_record|
+    user_record[:password] = create_hash_digest(user_record[:password])
+  end
+  list_of_users
+end
+
+puts create_secure_users(users)
